b7d3732ac220c3ad98bf324b2971d5decff2fc72
Jordana-Valentini-Pagliuso <47749394+Jordana-Valentini-Pagliuso@users.noreply.github.com>
2019-02-25 11:42:48 -0300

    Add files via upload

---
+++ js/funcao.js
@@ -0,0 +1,32 @@
+﻿function data(campo) {
+    if (campo.value.length == 2 || campo.value.length == 5) {
+        campo.value += "/";
+    }
+}
+$(document).ready(function(){
+
+    $("#calcular").click(
+
+        
+        function(){
+            let tema = parseFloat($("#tTema").val());
+            let bolo = parseFloat($("#tBolo").val());
+            let doce = parseFloat($("#tDoces").val());
+            let bebida = parseFloat($("#tBebidas").val());
+            let salgado = parseFloat($("#tSalgados").val());
+            let total ;
+
+            $("#tValorTema").val(tema);
+            $("#tValorBolo").val(bolo);
+            $("#tValorDoce").val(doce);
+            $("#tValorBebida").val(bebida);
+            $("#tValorSalgado").val(salgado);
+            $("#tValor").val();
+
+            total = tema + bolo + doce + bebida + salgado;
+
+            $("#tTotal").val(total);
+        }
+    );
+
+});
